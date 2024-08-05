89e5a76faa3f35656891f58037217d1b68740fa7
Worstik11 <sadistik22@gmail.com>
2024-08-06 01:00:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@
 
 
 module.exports = {
-  TOKEN: "MTI1OTEwOTQyNDI4MzM4NTg4Ng.G5KT0x.yiss5vDo_6k67vQ8ms9MqCvKTUx4aBTXOqq7W0",
+  TOKEN: "",
   ownerID: ["1004206704994566164", ""], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
