58b14a3a22fe82f561c41ff0d8f447234e1e6b14
Worstik11 <sadistik22@gmail.com>
2024-08-06 00:06:10 +0300

    Update pw.js

--- util/pw.js
+++ util/pw.js
@@ -9,9 +9,9 @@ function printWatermark() {
 
   console.log('\x1b[1m\x1b[36m╔════════════════════════════════════════════╗');
   console.log('\x1b[1m\x1b[36m║                                            ║');
-  console.log(`\x1b[1m\x1b[36m            ${botName}     `);
+  console.log(`\x1b[1m\x1b[36m            🚀 ${botName}     `);
   console.log(`\x1b[1m\x1b[36m            👑 Authorization : ${status}    `);
-  console.log(`\x1b[1m\x1b[36m            💡 Version: ${version}`);
+  console.log(`\x1b[1m\x1b[36m            💡  Version: ${version}`);
   console.log(`\x1b[1m\x1b[36m            📅 Uptime: ${uptimeInSeconds}s`);
   console.log(`\x1b[1m\x1b[36m            🚀 Powered by ${Text}`);
   console.log('\x1b[1m\x1b[36m║                                            ║');
